02c2509820a6af74e9fcde8f977394d82705ca39
YaronD <yarondrukh@gmail.com>
2024-05-06 17:41:03 +0300

    Add docs for question five

--- PartTwo/question_five.py
+++ PartTwo/question_five.py
@@ -2,5 +2,9 @@ from mpmath import mp
 
 
 def reverse_n_pi_digits(n: int):
+    """
+    Returns the amount of the given digits in pi from the start, but in reverse.
+    :param n: the amount if digits
+    """
     mp.dps = n + 1
     return (str(mp.pi)[2:2 + n])[::-1]
