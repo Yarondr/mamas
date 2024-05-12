9061652f73db0a814bdc8b2a66ca514bf9c28789
YaronD <yarondrukh@gmail.com>
2024-05-13 01:51:43 +0300

    Add node class (question five)

---
+++ PartThree/Node.cs
@@ -0,0 +1,12 @@
+namespace PartThree
+{
+    public class Node(int value, Node? next)
+    {
+        public int Value { get; set; } = value;
+        public Node? Next { get; set; } = next;
+
+        public Node(int value) : this(value, null)
+        {
+        }
+    }
+}
